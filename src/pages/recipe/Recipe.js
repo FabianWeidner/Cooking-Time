ef4abd025d45dc8847d08a906a6cac2bd16a9be0
Fabian Phillipe <fabianphillipe@gmail.com>
2021-11-08 14:58:31 +0100

    invoke firebase/firestore to recipe component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,13 +1,35 @@
-import { useParams } from 'react-router';
-import { useFetch } from '../../hooks/useFetch';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { useTheme } from '../../hooks/useTheme';
+import { projectFirestore } from '../../firebase/config';
+
 import './Recipe.scss';
 
 function Recipe() {
   const { id } = useParams();
-  const url = 'http://localhost:3000/recipes/' + id;
-  const { data: recipe, isPending, error } = useFetch(url);
   const { mode } = useTheme();
+
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError('Coul not find that recipe');
+        }
+      });
+  }, [id]);
 
   return (
     <div className={`recipe ${mode}`}>
